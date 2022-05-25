502443e652dd75f8a641dd330ca3fa3ee780c9bf
SAX_94 <matteosacchi94@gmail.com>
2022-05-25 22:19:02 +0200

    snack4 completed

---
+++ snack4/js/script.js
@@ -0,0 +1,16 @@
+const invitati = ["Brad Pitt", "Bill Burr", "Michael Jordan", "Bill Gates"]
+
+
+const output = document.querySelector(".page-cont");
+const nameSurname = prompt("Inserisci nome e cognome:")
+
+let found = false;
+let result = `<span class="red">Il tuo nome non è presente nella lista degli invitati.</span>`;
+
+for (let i = 0; i < invitati.length; i++) {
+    if (nameSurname === invitati[i]) found = true;
+}
+
+if (found) result = `<span class="green">Benvenuto alla festa del grande Gatsby!</span>`;
+
+output.innerHTML = `<h1>${result}</h1>`;
